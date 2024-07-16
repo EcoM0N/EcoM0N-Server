9a2d5824dcaced2a6b1968ca33c42bf852c35a94
se0ngeun <se0ngeun@dgsw.hs.kr>
2024-07-16 17:28:41 +0900

    first commit

---
+++ src/main/java/org/example/ecom0n/EcoM0NApplication.java
@@ -0,0 +1,13 @@
+package org.example.ecom0n;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoM0NApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcoM0NApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/ecom0n/EcoM0NApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ecom0n;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoM0NApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
